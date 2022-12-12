9948c4048c337db4b87dc597ca290540a20a85fe
Ewout Quax <ewout.quax@bettyblocks.com>
2022-12-12 08:19:43 +0100

    Refactor day-01: move files to new package
    Code is starting to look better

--- day-01-calorie-counting/main.go
+++ day-01-calorie-counting/main.go
@@ -1,11 +1,10 @@
 package main
 
 import (
+	"fmt"
+
 	"aoc.com/2022/day-01/puzzle"
 	"aoc.com/2022/day-01/utils"
-	"fmt"
-	"os"
-	"strings"
 )
 
 func main() {
@@ -14,42 +13,9 @@ func main() {
 }
 
 func solvePart1() int {
-	var blocks [][]string = readFileAsBlocks()
-
-	return puzzle.TotalTopN(blocks, 1)
+	return puzzle.TotalTopN(utils.ReadFileAsBlocks(), 1)
 }
 
 func solvePart2() int {
-	var blocks [][]string = readFileAsBlocks()
-	return puzzle.TotalTopN(blocks, 3)
-}
-
-func readFileAsNumbers() (numbers []int) {
-	var lines []string = readFileAsLines()
-
-	for _, string := range lines {
-		numbers = append(numbers, utils.ConvStrToI(string))
-	}
-
-	return
-}
-
-func readFileAsBlocks() (blocks [][]string) {
-	var block_inputs []string = strings.Split(readFile(), "\n\n")
-
-	for _, block_input := range block_inputs {
-		blocks = append(blocks, strings.Split(block_input, "\n"))
-	}
-	return
-}
-
-func readFileAsLines() []string {
-	return strings.Split(readFile(), "\n")
-}
-
-func readFile() string {
-	raw, err := os.ReadFile("input.txt")
-	utils.Check(err)
-
-	return strings.TrimSuffix(string(raw), "\n")
+	return puzzle.TotalTopN(utils.ReadFileAsBlocks(), 3)
 }
